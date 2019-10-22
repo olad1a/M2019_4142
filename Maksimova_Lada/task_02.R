dfe1cbf11925b52df22210bcf96a1a92b730797b
olad1a <56296602+olad1a@users.noreply.github.com>
2019-10-22 20:35:52 +0300

    Add files via upload

---
+++ Maksimova_Lada/task_02.R
@@ -0,0 +1,26 @@
+#my function
+
+basic_calculations <- function (data, r_s, c_s) {
+  subset <- data[r_s, c_s]
+  my_list <- list(subset)
+  for (i in 1:length(subset)){
+    if (is.numeric(subset[ , i])){
+      result1 <- mean(subset[ , i])
+      my_list <- append(my_list, result1)
+      
+    } else {
+      result2 <- table(subset[ , i])
+      my_list <- append(my_list, result2)
+    }}
+  print(my_list)} 
+
+#examples 
+
+#1
+basic_calculations(iris, 1:15, 1:5)
+
+#2
+basic_calculations(iris, c(1, 35, 67, 106, 145), c(1, 2, 5))
+
+#tree
+basic_calculations(trees, c(1:5, 8, 21), 1:3)
